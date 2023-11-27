910afc933587cb258e32b2f4b68cb6296907b322
CodeBernardo <stein.bernardo@proton.me>
2023-11-27 10:12:37 -0300

    feat: create product interface & CRUDOperations

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import { Product, CRUDOperations } from "./interfaces";
+
+// class productList implements Product, CRUDOperations {
+//   private productList: Product[] = [];
+//   id = 1;
+  
+// }
---
+++ src/interfaces.ts
@@ -0,0 +1,21 @@
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type CreateProduct = Pick<Product, "name" | "price">;
+type UpdateProduct = Partial<CreateProduct>;
+type DeleteResult = { message: string };
+
+interface CRUDOperations {
+  createProduct(product: CreateProduct): Product;
+  getProducts(): Product[];
+  getOneProduct(id: number): Product | undefined;
+  updateProduct(id: number, data: UpdateProduct): Product;
+  deleteProduct(id: number): DeleteResult;
+}
+
+export { Product, CRUDOperations };
